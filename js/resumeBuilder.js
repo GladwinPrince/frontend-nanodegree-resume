37f0878668fd673f9e0bd15963708d79604cfcc7
Unknown <GladwinPrinceC6902@gmail.com>
2017-07-23 14:40:46 +0530

    Added Extra Resume Data
    Added Work Experience
    Added Education Details

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -10,6 +10,23 @@ var bio={
     "welcomeMsg":"Hi, I am Gladwin Prince",
     "skills":["Java","Java Servlets","Java Servlet Pages (JSP)","HyperText Markup Language (HTML)","Cascading Stylesheets (CSS)","JavaScript"]
 };
+
+var work={
+    "employer":"Udacity",
+    "title":"Front-End Web Developer",
+    "duration":"3 years",
+    "location":"Madurai, Tamil Nadu, India",
+    "description":"I design Web pages :)"
+};
+
+var education={
+    "name":"Karunya University",
+    "degree":"B.Tech",
+    "duration":"4 years",
+    "location":"Coimbatore, Tamil Nadu, India",
+    "major":"Electronics and Communications"
+};
+
 var bioName=HTMLheaderName.replace("%data%",bio.name);
 var bioRole=HTMLheaderRole.replace("%data%",bio.role);
 var bioMobile=HTMLmobile.replace("%data%",bio.mobile);
@@ -23,6 +40,19 @@ var bioSkills=[];
 for(var i=0;i<bio.skills.length;i++){
      bioSkills.push(HTMLskills.replace("%data%",bio.skills[i]));
 }
+
+var workEmployer=HTMLworkEmployer.replace("%data%",work.employer);
+var workTitle=HTMLworkTitle.replace("%data%",work.title);
+var workDuration=HTMLworkDates.replace("%data%",work.duration);
+var workLocation=HTMLworkLocation.replace("%data%",work.location);
+var workDescription=HTMLworkDescription.replace("%data%",work.description);
+
+var schoolName=HTMLschoolName.replace("%data%",education["name"]);
+var schoolDegree=HTMLschoolDegree.replace("%data%",education["degree"]);
+var schoolDuration=HTMLschoolDates.replace("%data%",education["duration"]);
+var schoolLocation=HTMLschoolLocation.replace("%data%",education["location"]);
+var schoolMajor=HTMLschoolMajor.replace("%data%",education["major"]);
+
 $("#header").prepend(bioRole);
 $("#header").prepend(bioName);
 $("#header").append(bioPicture);
@@ -41,3 +71,17 @@ $("#footerContacts").append(bioMail);
 $("#footerContacts").append(bioTwitter);
 $("#footerContacts").append(bioGithub);
 $("#footerContacts").append(bioLocation);
+
+$("#workExperience").append(HTMLworkStart);
+$(".work-entry").append(workEmployer);
+$(".work-entry").append(workTitle);
+$(".work-entry").append(workDuration);
+$(".work-entry").append(workLocation);
+$(".work-entry").append(workDescription);
+
+$("#education").append(HTMLschoolStart);
+$(".education-entry").append(schoolName);
+$(".education-entry").append(schoolDegree);
+$(".education-entry").append(schoolDuration);
+$(".education-entry").append(schoolLocation);
+$(".education-entry").append(schoolMajor);
