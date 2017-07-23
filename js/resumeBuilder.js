721992aa7b851784e569fbf5d2e64a6d8a8d16f2
Unknown <GladwinPrinceC6902@gmail.com>
2017-07-23 12:12:04 +0530

    Header Name & Role Addition

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,4 @@
-$("#main").append("Gladwin Prince");
+var formattedName=HTMLheaderName.replace("%data%","Gladwin Prince");
+var formattedRole=HTMLheaderRole.replace("%data%","Front-End Web Developer");
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
